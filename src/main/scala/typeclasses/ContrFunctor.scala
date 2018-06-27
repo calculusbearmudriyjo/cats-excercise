920760eba86b10a16250d702dfc1e81da4ee189a
Alexander Babin <mudriyjo@gmail.com>
2018-06-27 17:44:57 +0300

    generalize coFunctor

--- src/main/scala/typeclasses/ContrFunctor.scala
+++ src/main/scala/typeclasses/ContrFunctor.scala
@@ -13,8 +13,7 @@ object ContrFunctor {
   implicit val stringPrintable: Printable[String] = (value: String) => "\"" + value + "\""
   implicit val booleanPrintable: Printable[Boolean] = (value: Boolean) => if(value) "yes" else "no"
 
-  implicit val boxStringPrintable: Printable[Box[String]] = stringPrintable.contraMap((x: Box[String]) => x.value)
-  implicit val boxBooleanPrintable: Printable[Box[Boolean]] = booleanPrintable.contraMap((x: Box[Boolean]) => x.value)
+  implicit def boxStringPrintable[A](implicit printable: Printable[A]): Printable[Box[A]] = stringPrintable.contraMap((x: Box[A]) => format(x.value))
 
   final case class Box[A](value: A)
 
