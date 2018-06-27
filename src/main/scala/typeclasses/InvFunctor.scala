a80cfae5002f1885c02cdf8f015231eccc64410d
Alexander Babin <mudriyjo@gmail.com>
2018-06-27 17:29:41 +0300

    add example with generalize function codec

--- src/main/scala/typeclasses/InvFunctor.scala
+++ src/main/scala/typeclasses/InvFunctor.scala
@@ -23,10 +23,29 @@ object InvFunctor {
     }
   }
 
+  implicit def codecBox[A](implicit b: Codec[A]): Codec[Box[A]] = b.imap(x => Box(x), x => x.value)
+
+  case class Box[A](value: A)
+
   def main(args: Array[String]): Unit = {
-    implicit val codec: Codec[Boolean] = stringCodec.imap(_.toBoolean, _.toString)
+    implicit val codecBool: Codec[Boolean] = stringCodec.imap(_.toBoolean, _.toString)
+    implicit val codecDouble: Codec[Double] = stringCodec.imap(_.toDouble, _.toString)
 
     println(encode(true))
     println(decode[Boolean]("true"))
+    println(encode(2.2))
+    println(decode[Double]("2.2"))
+
+    println(encode(123.4))
+    // res0: String = 123.4
+    println(decode[Double]("123.4"))
+    // res1: Double = 123.4
+    println(encode(Box(123.4)))
+    // res2: String = 123.4
+    println(decode[Box[Double]]("123.4"))
+    // res3: Box[Double] = Box(123.4)
+    println(encode(Box(true)))
+    // res2: String = 123.4
+    println(decode[Box[Boolean]]("false"))
   }
 }
